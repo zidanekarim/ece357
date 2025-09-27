c901d140f148a7560d9842698ded5c605938af99
Zidane Karim <zkarim7676@gmail.com>
2025-09-27 13:31:43 -0400

    worked on simplefind

--- pset02/simplefind.c
+++ pset02/simplefind.c
@@ -2,6 +2,20 @@
 
 
 
-char** search(char* pattern, char* path, bool verbose, bool x) {
-    
+struct entry** search(char* pattern, char* path, bool verbose, bool x) {
+    DIR* directory = opendir(path);
+    if (directory == NULL) return NULL;
+
+    struct dirent* dir_entry;
+    struct entry** results = NULL;
+    size_t count = 0;
+
+    // now we use readdir to read contents of directory
+    while ((dir_entry = readdir(directory)) != NULL) {
+        if (fnmatch(dir_entry->d_name, ".", 0) == 0 || fnmatch(dir_entry->d_name, "..", 0) == 0) {
+            continue; 
+        }
+        if (fnmatch(dir_entry->d_name, pattern, 0))
+
+
 }
