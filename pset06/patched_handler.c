8a125faaaf92a66ae4c3ca277a43209c515aac2a
Zidane Karim <zkarim7676@gmail.com>
2025-11-30 18:36:20 -0500

    question 1

---
+++ pset06/patched_handler.c
@@ -0,0 +1,24 @@
+struct shared_data_struct
+{
+    int lock;
+    /* Other data structures stuff */
+} *ms;
+/* signal_handler is the disposition of SIGINT */
+void signal_handler(int sig)
+{
+    spin_lock(&ms->lock);
+    /* Manipulate data structure */
+    ms->lock = 0;
+}
+int some_function(int a)
+{
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    spin_lock(&ms->lock);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask); // now SIGINT is blocked until the lock is released
+    /* Manipulate data structure */
+    ms->lock = 0;
+    sigprocmask(SIG_SETMASK, &oldmask, NULL); // restore old mask
+    return something;
+}
