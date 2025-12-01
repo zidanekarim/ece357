c2bb519e13b5b46206aa940643e26b5fcb4f8bf1
Zidane Karim <zkarim7676@gmail.com>
2025-12-01 11:25:53 -0500

    downlaoded files

--- pset06/p1/patched_handler.c
+++ pset06/p1/patched_handler.c
@@ -16,7 +16,7 @@ int some_function(int a)
     sigemptyset(&mask);
     sigaddset(&mask, SIGINT);
     spin_lock(&ms->lock);
-    sigprocmask(SIG_BLOCK, &mask, &oldmask); // now SIGINT is blocked until the lock is released
+    sigprocmask(SIG_BLOCK, &mask, &oldmask); // now SIGINT is blocked until the lock is released, so handler won't run and deadlock
     /* Manipulate data structure */
     ms->lock = 0;
     sigprocmask(SIG_SETMASK, &oldmask, NULL); // restore old mask
